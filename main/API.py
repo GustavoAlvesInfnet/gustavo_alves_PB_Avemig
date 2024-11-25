c3977ead06029b5a505410d5f1be50d418f3fb4c
“dsCarneiro03@gmail.com” <dscarneiro03@gmail.com>
2024-11-24 21:33:56 -0300

    Finalização da API requisitada no TP4

--- main/API.py
+++ main/API.py
@@ -1,5 +1,5 @@
 import csv
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from pydantic import BaseModel
 import pandas as pd
 from fastapi import Query
@@ -7,6 +7,7 @@ from fastapi import Query
 from transformers import pipeline
 
 import subprocess
+from fastapi.responses import JSONResponse
 
 def run_uvicorn():
     comando = "uvicorn main.API:app --reload"
@@ -82,17 +83,50 @@ async def criar_nova_especie(request: Request):
 
 # curl http://localhost:8000/avesAPI/todas?category=AvesTeste
 
+# LLM - TP4
+curiosity_context = """Example of response: Here are some curious things about birds:
+\n-Crows and ravens
+\nThese birds are known to be curious and intelligent. They have been observed digging clams at low tide and using traffic to crush them. They have also been seen following traffic. 
+\n-Keas
+\nThese birds are known to be inquisitive and playful, and they manipulate objects throughout their lives. 
+\n-Evolution
+\nBirds evolved from theropod dinosaurs during the Jurassic period, around 165–150 million years ago. 
+\n-Origin of birds
+\nThe scientific consensus is that birds are a group of maniraptoran theropod dinosaurs."""
 
-# curl http://localhost:8000/avesAPI/curiosidade
+class CuriosityResponse(BaseModel):
+    curiosity: str
+
 @app.get("/avesAPI/curiosidade")
 async def curiosity_llm():
-    pipe = pipeline("text-generation", model="distilbert/distilgpt2")
-    result = pipe("A birds curiosity:",truncation=True, max_length=120)
-    result = result[0]["generated_text"]
-    result = result.replace("\n", "")
+    try:
+        pipe = pipeline("text-generation", model="distilbert/distilgpt2")
+        question = f"{curiosity_context}. A curiosity of birds:"
+        result = pipe(question, truncation=True, max_length=275)
+        result = result[0]["generated_text"]
+        result = result.replace(question, "")
+        result = result.replace("\n", "")
 
-    #faz com que a Ia termine em ponto final
-    result = result + "."
-    return result
+        # Crie um objeto CuriosityResponse com a resposta gerada
+        response = CuriosityResponse(curiosity=result)
 
-
+        # Retorne o objeto CuriosityResponse como uma resposta JSON
+        return response
+    except Exception as e:
+        # Trate o erro e retorne um código de erro adequado
+        if isinstance(e, ConnectionError):
+            raise HTTPException(status_code=503, detail="Serviço Indisponível")
+        elif isinstance(e, TimeoutError):
+            raise HTTPException(status_code=504, detail="Tempo de resposta excedido")
+        else:
+            raise HTTPException(status_code=500, detail="Erro interno do servidor")
+    
+@app.exception_handler(Exception)
+async def handle_exception(request, exc):
+    # Trate o erro e retorne um código de erro adequado
+    if isinstance(exc, ConnectionError):
+        return JSONResponse(status_code=503, content={"detail": "Serviço Indisponível"})
+    elif isinstance(exc, TimeoutError):
+        return JSONResponse(status_code=504, content={"detail": "Tempo de resposta excedido"})
+    else:
+        return JSONResponse(status_code=500, content={"detail": "Erro interno do servidor"})
